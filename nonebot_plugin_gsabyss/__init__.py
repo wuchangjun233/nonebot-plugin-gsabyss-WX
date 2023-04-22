f196fd37e31844f5a61859790f96e196cc3c33d7
wuchangjun233 <2101546835@qq.com>
2023-04-22 13:04:40 +0800

    🎉 支持OneBot_v12

--- nonebot_plugin_gsabyss/__init__.py
+++ nonebot_plugin_gsabyss/__init__.py
@@ -1,6 +1,9 @@
+import time
+
+from nonebot import get_bot
 from nonebot.params import CommandArg
 from nonebot.plugin import on_command
-from nonebot.adapters.ntchat import Message, MessageSegment
+from nonebot.adapters.onebot.v12 import Bot, Message, MessageEvent
 
 from .config import plugin_config
 from .draw_quickview import AbyssQuickViewDraw
@@ -8,22 +11,55 @@ from .draw_statistic import AbyssStatisticDraw
 from .data_source import fetch_akasha_abyss, parse_quickview_input
 
 PRIORITY = plugin_config.gsabyss_priority
-quickview_matcher = on_command("速览", aliases={"深渊速览"}, priority=PRIORITY, block=True)
+quickview_matcher = on_command("速览",
+                               aliases={"深渊速览"},
+                               priority=PRIORITY,
+                               block=True)
 totalview_matcher = on_command("深渊统计", priority=PRIORITY, block=True)
 
 
+async def file_upload(data: bytes, file_name: str):
+    bot = get_bot()
+    up_data = await bot.call_api(
+                    'upload_file',
+                    type="data",
+                    data=data,
+                    name=f"{file_name}",
+                )
+    file_id = up_data['file_id']
+    return file_id
+
+
 @quickview_matcher.handle()
-async def abyssQuick(arg: Message = CommandArg()):
+async def abyssQuick(bot: Bot, ev: MessageEvent, arg: Message = CommandArg()):
     floor_idx, chamber_idx, schedule_key = parse_quickview_input(str(arg))
     drawer = AbyssQuickViewDraw(floor_idx, chamber_idx, schedule_key)
     res = await drawer.get_full_picture()
+
+    async def send_file_message(res, file_type):
+        params = {}
+        if ev.detail_type == "group":
+            params["detail_type"] = "group"
+            params["group_id"] = ev.group_id
+        elif ev.detail_type == "private":
+            params["detail_type"] = "private"
+            params["user_id"] = ev.user_id
+        timestamp = time.time()
+        file_name = f'_{timestamp}.png'
+        file_id = await file_upload(res, file_name)
+        params["message"] = [
+            {"type": file_type, "data": {"file_id": file_id}}
+        ]
+        await bot.call_api('send_message', **params)
+
     await quickview_matcher.finish(
-        res if isinstance(res, str) else MessageSegment.image(res)
-    )
+            res if isinstance(res, str)
+            else await send_file_message(res.getvalue(), "image")
+        )
 
 
 @totalview_matcher.handle()
-async def abyssTotal(arg: Message = CommandArg()):
+async def abyssTotal(bot: Bot, ev: MessageEvent, arg: Message = CommandArg()):
     if arg:
         await totalview_matcher.finish()
     akasha_data = await fetch_akasha_abyss()
@@ -31,4 +67,24 @@ async def abyssTotal(arg: Message = CommandArg()):
         await totalview_matcher.finish(akasha_data)
     drawer = AbyssStatisticDraw(akasha_data)
     res = await drawer.get_full_picture()
-    await totalview_matcher.finish(MessageSegment.image(res))
+
+    async def send_file_message(res, file_type):
+        params = {}
+        if ev.detail_type == "group":
+            params["detail_type"] = "group"
+            params["group_id"] = ev.group_id
+        elif ev.detail_type == "private":
+            params["detail_type"] = "private"
+            params["user_id"] = ev.user_id
+        timestamp = time.time()
+        file_name = f'_{timestamp}.png'
+        file_id = await file_upload(res, file_name)
+        params["message"] = [
+            {"type": file_type, "data": {"file_id": file_id}}
+        ]
+        await bot.call_api('send_message', **params)
+
+    await totalview_matcher.finish(
+            res if isinstance(res, str)
+            else await send_file_message(res.getvalue(), "image")
+        )
